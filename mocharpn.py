d2c4ab17e55e6b36e1464f3bda3d1e0fb715f1ab
Mocha <mocha2007andcoco@gmail.com>
2018-04-06 20:50:35 -0400

    code enhancements

--- mocharpn.py
+++ mocharpn.py
@@ -5,6 +5,7 @@ def rpn(prog):
 	stack = []
 	cnum = ''
 	for command in prog:
+		err = 'ERR @ '+command
 		if command in '01234567890':
 			cnum += command
 		elif command == '.':
@@ -26,92 +27,62 @@ def rpn(prog):
 			cnum = ''
 
 			if len(stack): # for commands requiring at least ONE number
-				if command in ', ':
-					pass
+				temp = stack.pop()
+				if command in ' \t\n':
+					stack.append(temp)
 				elif command == '~':
-					temp = stack.pop()
 					stack.append(-temp)
 				elif command == 'c':
-					temp = stack.pop()
 					stack.append(cos(temp))
 				elif command == 'C':
-					temp = stack.pop()
 					stack.append(acos(temp))
 				elif command == 'l':
-					temp = stack.pop()
 					stack.append(log(temp))
 				elif command == 's':
-					temp = stack.pop()
 					stack.append(sin(temp))
 				elif command == 'S':
-					temp = stack.pop()
 					stack.append(asin(temp))
 				elif command == 't':
-					temp = stack.pop()
 					stack.append(tan(temp))
 				elif command == 'T':
-					temp = stack.pop()
 					stack.append(atan(temp))
 				elif command == '[':
-					temp = stack.pop()
 					stack.append(floor(temp))
 				elif command == ']':
-					temp = stack.pop()
 					stack.append(ceil(temp))
 				elif command == '|':
-					temp = stack.pop()
 					stack.append(abs(temp))
 				# stack manip
 				elif command == ';':
-					stack.pop()
+					pass
+				elif command == ',':
+					stack += list(range(temp))
 				elif command == '@':
-					temp = stack.pop()
 					stack = [temp] + stack
-				elif len(stack)>=2: # for commands requiring at least ONE number
+				elif len(stack): # for commands requiring at least TWO numbers
+					temp2 = stack.pop()
 					if command == '+':
-						temp = stack.pop()
-						temp2 = stack.pop()
 						stack.append(temp2+temp)
 					elif command == '-':
-						temp = stack.pop()
-						temp2 = stack.pop()
 						stack.append(temp2-temp)
 					elif command == '*':
-						temp = stack.pop()
-						temp2 = stack.pop()
 						stack.append(temp2*temp)
 					elif command == '/':
-						temp = stack.pop()
-						temp2 = stack.pop()
-						if temp == 0:return 'Nice try, sweetheart.'
+						if temp == 0:return err
 						stack.append(temp2/temp)
 					elif command == '%':
-						temp = stack.pop()
-						temp2 = stack.pop()
-						if temp == 0:return 'Nice try, sweetheart.'
+						if temp == 0:return err
 						stack.append(temp2%temp)
 					elif command == '^':
-						temp = stack.pop()
-						temp2 = stack.pop()
-						if temp == temp2 == 0:return 'Nice try, sweetheart.'
+						if temp == temp2 == 0:return err
 						stack.append(temp2**temp)
 					elif command == 'L':
-						temp = stack.pop()
-						temp2 = stack.pop()
 						stack.append(log(temp)/log(temp2))
 					# stack manip
 					elif command == '\\':
-						temp = stack.pop()
-						temp2 = stack.pop()
 						stack.append(temp)
 						stack.append(temp2)
-					else:
-						stack = 'ERR @ '+command
-						break
-				else:
-					stack = 'ERR @ '+command
-					break
-			else:
-				stack = 'ERR @ '+command
-				break
+					else:return err
+				else:return err
+			else:return err
 	return stack
