3599931f8567db00965b0026a701db0a05a63edb
Mocha <mocha2007andcoco@gmail.com>
2019-08-14 23:23:54 -0400

    mochatest cleanup

--- mochatest.py
+++ mochatest.py
@@ -1,16 +1,19 @@
-from random import choice as c
-from random import randint as r
+from random import choice, randint
+from typing import Tuple
 from mochaxyz import grammarq
 
-def wilson(u,d):
+
+def wilson(u: float, d: float) -> float:
 	z = 1.95 # 95% confidence
 	n = u+d
 	a = (u+z**2/2)/(n+z**2)
 	b = z/(n+z**2)*(u*d/n+z**2/4)**.5
 	return a-b
 
-def rpair():
-	return r(0,12),r(0,12)
 
-def rgrammar():
-	return c(grammarq)
+def rpair() -> Tuple[int, int]:
+	return randint(0, 12), randint(0, 12)
+
+
+def rgrammar() -> Tuple[str, str]:
+	return choice(grammarq)
