028e5970fac27f4282eb2beb3aac4dc3af4d10a6
Mocha <mocha2007andcoco@gmail.com>
2019-08-14 22:59:48 -0400

    mochabf cleanup

--- mochabf.py
+++ mochabf.py
@@ -1,53 +1,75 @@
-from time import time as t
-
-def tonum(x):
-	try:return int(x)
-	except:	
-		try:return chr(x)
-		except:return len(x)
+from time import time
+from typing import List
 
 mem = [0]*256
 
-def run(prog,inp):
-	inp = list(map(tonum,inp))
+
+def tonum(x: str) -> int:
+	try:
+		return int(x)
+	except ValueError:
+		pass
+	if 1 == len(x):
+		return ord(x)
+	else:
+		return len(x)
+
+
+def run(prog: str, inp: List[str]) -> str:
+	inp = list(map(tonum, inp))
 	o = []
 	inpn = 0
-	start = t()
+	start = time()
 	pointer = 0
 	i = 0
 	while 1:
-		try:command = prog[i]
-		except:break
+		try:
+			command = prog[i]
+		except IndexError:
+			break
 		
-		if command == '>':pointer+=1
-		elif command == '<':pointer-=1
-		elif command == '+':mem[pointer]+=1
-		elif command == '-':mem[pointer]-=1
+		if command == '>':
+			pointer += 1
+		elif command == '<':
+			pointer -= 1
+		elif command == '+':
+			mem[pointer] += 1
+		elif command == '-':
+			mem[pointer] -= 1
 		elif command == ',':
-			try:mem[pointer]=inp[inpn]
-			except:return 'Error: End of input'
-			inpn+=1
-		elif command == '.':o+=[mem[pointer]]
+			try:
+				mem[pointer] = inp[inpn]
+			except IndexError:
+				return 'Error: End of input'
+			inpn += 1
+		elif command == '.':
+			o += [mem[pointer]]
 		elif command == '[':# jump to the next matching ]
 			if mem[pointer] == 0:
 				balance = 1
 				while balance:
-					i+=1
+					i += 1
 					try:
-						if prog[i] == '[':balance+=1
-						elif prog[i] == ']':balance-=1
-					except:return 'Error: End of input'
-				i-=1
-		elif command == ']':# jump to the next matching [
+						if prog[i] == '[':
+							balance += 1
+						elif prog[i] == ']':
+							balance -= 1
+					except IndexError:
+						return 'Error: End of input'
+				i -= 1
+		elif command == ']': # jump to the next matching [
 			if mem[pointer]:
 				balance = -1
 				while balance:
-					i-=1
-					if prog[i] == '[':balance+=1
-					elif prog[i] == ']':balance-=1
+					i -= 1
+					if prog[i] == '[':
+						balance += 1
+					elif prog[i] == ']':
+						balance -= 1
 					
-		pointer = pointer%256
-		mem[pointer] = mem[pointer]%256
-		i+=1
-		if t()-start>5:return 'Took too long (5s) Stopped at char '+str(i)
-	return str(o)+'\n'+''.join(map(chr,o))
+		pointer = pointer % 256
+		mem[pointer] = mem[pointer] % 256
+		i += 1
+		if 5 < time()-start:
+			return 'Took too long (5s) Stopped at char '+str(i)
+	return str(o)+'\n'+''.join(map(chr, o))
